2d14740d6f65aae5af49582d13b39b5604adb6fb
AliMazhar110 <alimazharluqmani786@gmail.com>
2022-02-02 21:08:46 +0530

    insertion sort

--- Sorting-Algorithms/Insertion-Sort.cpp
+++ Sorting-Algorithms/Insertion-Sort.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+void insertion_sort(int *arr, int n){
+    
+}
+
+void display(int arr[], int n){
+    cout << "\nArray = ";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(void){
+    int n;
+    cout << "Total number of elements = ";
+    cin >> n;
+    int arr[n];
+    cout << endl;
+    for(int i=0;i<n;i++){
+        cout << "Enter A Number = ";
+        cin >> arr[i];
+    }
+    display(arr,n);
+    insertion_sort(arr,n);
     return 0;
 }
