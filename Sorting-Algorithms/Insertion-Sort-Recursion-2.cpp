8a3d5cdd2c87bfb9b1ff0b7e3e7711d94daa4995
AliMazhar110 <alimazharluqmani786@gmail.com>
2022-02-07 20:54:13 +0530

    recursion

---
+++ Sorting-Algorithms/Insertion-Sort-Recursion-2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void display(int arr[], int n){
+    cout << "\nArray = ";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+}
+
+void insertion_sort(int arr[], int n){
+
+}
+
+void get_element(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout << "Enter A Number = ";
+        cin >> arr[i];
+    }
+}
+
+int main(void){
+    int n;
+    cout << "Total number of elements = ";
+    cin >> n;
+    int arr[n];
+    cout << endl;
+    get_element(arr,n);
+    display(arr,n);
+    insertion_sort(arr,n);
+    return 0;
+}
